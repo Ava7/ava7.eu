28a8a43f69949000d99d0495350a23a143d120e7
Ava <avalanche_1991@hotmail.com>
2015-09-01 20:59:42 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Ava7.EU
+
+Just a simple static page, nothing really fancy... you can go watch something more exciting
---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>ava7.eu</title>
+    <meta name="description" content="ava7.eu - personal website" />
+    <meta name="keywords" content="php, mysql, javascript, computers, ava, avalanche, web, programming" />
+    <meta name="googlebot" content="index, follow" />
+    <meta name="robots" content="index, follow" />
+
+    <link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="icon" href="http://www.ava7.eu/favicon.ico" type="image/x-icon"/>
+    <link rel="shortcut icon" href="http://www.ava7.eu/favicon.ico" type="image/x-icon"/>
+
+    <script type="text/javascript" src="js/modernizr.custom.79569.js"></script>
+    <script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="js/jquery.animate-colors-min.js"></script>
+    <script type="text/javascript" src="js/scripts.js"></script>
+    <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-25386265-1']);
+        _gaq.push(['_trackPageview']);
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="#" data-colour="#663366" title="about" data-for="about" id="page-about">about</a></li>
+            <li><a href="#" data-colour="#00966E" title="skills">skills</a></li>
+            <li><a href="#" data-colour="#008ac1" title="projects">projects</a></li>
+            <li><a href="#" data-colour="#225C65" title="message me">message me</a></li>
+        </ul>
+    </nav>
+
+    <section>
+        <article id="about">
+            <h2>About</h2>
+            <iframe width="900" height="506" src="//www.youtube-nocookie.com/embed/Y7-YO9twpdM" frameborder="0" allowfullscreen></iframe>
+        </article>
+    </section>
+
+</body>
+</html>
